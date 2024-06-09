c67b3e8e053a2a3b52f2398533b96209f716a78e
Cj-bc <cj.bc-sd@outlook.jp>
2024-06-10 02:18:27 +0900

    feat: support runtime value modification for UGUI Vector3 ValueStore

--- XRDevcomfy/RuntimeInspector/Scripts/ValueStore/UguiVector3ValueStore.cs
+++ XRDevcomfy/RuntimeInspector/Scripts/ValueStore/UguiVector3ValueStore.cs
@@ -5,12 +5,33 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 **/
 using UnityEngine;
 using TMPro;
+using System;
 
 public class UguiVector3ValueStore : Vector3ValueStore
 {
     [SerializeField] TMP_InputField x;
     [SerializeField] TMP_InputField y;
     [SerializeField] TMP_InputField z;
+
+    void OnEnable()
+    {
+	x.onSelect.AddListener((_) => isValueModificationOnGoing = true);
+	x.onDeselect.AddListener((s) => UpdateValue(s, (v, old) => new Vector3(v, old.y, old.z)));
+	y.onSelect.AddListener((_) => isValueModificationOnGoing = true);
+	y.onDeselect.AddListener((s) => UpdateValue(s, (v, old) => new Vector3(old.x, v, old.z)));
+	z.onSelect.AddListener((_) => isValueModificationOnGoing = true);
+	z.onDeselect.AddListener((s) => UpdateValue(s, (v, old) => new Vector3(old.x, old.y, v)));
+    }
+
+    void UpdateValue(string newValue, Func<float, Vector3, Vector3> converter)
+    {
+	isValueModificationOnGoing = false;
+	if (float.Parse(newValue) is float v)
+	{
+	    var currentValue = (Vector3)Getter();
+	    Setter(converter(v, currentValue));
+	}
+    }
 
     protected override bool SetValue(object newValue)
     {

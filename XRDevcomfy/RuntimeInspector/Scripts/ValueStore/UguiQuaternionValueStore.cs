b2e437d97cad97e61f4b523a985a07e0bbb16950
Cj-bc <cj.bc-sd@outlook.jp>
2024-06-10 00:04:44 +0900

    feat: Add toggle button to display Quaternion as Euler angle

--- XRDevcomfy/RuntimeInspector/Scripts/ValueStore/UguiQuaternionValueStore.cs
+++ XRDevcomfy/RuntimeInspector/Scripts/ValueStore/UguiQuaternionValueStore.cs
@@ -4,19 +4,37 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 **/
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UguiQuaternionValueStore : QuaternionValueStore
 {
     [Header("Euler Angles")]
+    [SerializeField] RectTransform eulerRepresentation;
     [SerializeField] TMP_Text eulerX;
     [SerializeField] TMP_Text eulerY;
     [SerializeField] TMP_Text eulerZ;
     [Header("Quaternion Angles")]
+    [SerializeField] RectTransform quaternionRepresentation;
     [SerializeField] TMP_Text quaternionX;
     [SerializeField] TMP_Text quaternionY;
     [SerializeField] TMP_Text quaternionZ;
     [SerializeField] TMP_Text quaternionW;
+    [SerializeField] Button toggleEuler;
+    bool showingAsEuler = true;
+
+    void OnEnable()
+    {
+	toggleEuler.onClick.AddListener(ToggleEuler);
+	ToggleEuler();
+    }
+
+    void ToggleEuler()
+    {
+	showingAsEuler = !showingAsEuler;
+	eulerRepresentation.gameObject.SetActive(showingAsEuler);
+	quaternionRepresentation.gameObject.SetActive(!showingAsEuler);
+    }
 
     protected override bool SetValue(object newValue)
     {

59b70236cc4312169bd401c48535a8359f08d0f2
Cj-bc <cj.bc-sd@outlook.jp>
2024-06-09 14:28:35 +0900

    feat!: Rename TMProPropertyTemplate to TMProValueStore

--- XRDevcomfy/Scripts/RuntimeInspector/TMProValueStore.cs
+++ XRDevcomfy/Scripts/RuntimeInspector/TMProValueStore.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class TMProPropertyTemplate : ValueStore
+public class TMProValueStore : ValueStore
 {
     [SerializeField] private TMP_Text propertyName;
     [SerializeField] private TMP_InputField valueField;

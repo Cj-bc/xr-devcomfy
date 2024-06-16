a2edda3e93356829fef8b3e6a3f775d522faa036
Cj-bc <cj.bc-sd@outlook.jp>
2024-06-16 14:27:09 +0900

    feat: UguiQuaternionValueStore support value modification

--- XRDevcomfy/RuntimeInspector/Scripts/ValueStore/UguiQuaternionValueStore.cs
+++ XRDevcomfy/RuntimeInspector/Scripts/ValueStore/UguiQuaternionValueStore.cs
@@ -3,6 +3,7 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 **/
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -38,6 +39,36 @@ public class UguiQuaternionValueStore : QuaternionValueStore
 
     protected override void SetupValueModificationListeners()
     {
+	eulerX.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	eulerX.onValueChanged.AddListener(s => UpdateValue(showingAsEuler, s, (v, old) => Quaternion.Euler(v, old.eulerAngles.y, old.eulerAngles.z)));
+	eulerY.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	eulerY.onValueChanged.AddListener(s => UpdateValue(showingAsEuler, s, (v, old) => Quaternion.Euler(old.eulerAngles.x, v, old.eulerAngles.z)));
+	eulerZ.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	eulerZ.onValueChanged.AddListener(s => UpdateValue(showingAsEuler, s, (v, old) => Quaternion.Euler(old.eulerAngles.x, old.eulerAngles.y, v)));
+
+	quaternionX.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	quaternionX.onValueChanged.AddListener(s => UpdateValue(!showingAsEuler, s, (v, old) => new Quaternion(v, old.y, old.z, old.w)));
+	quaternionY.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	quaternionY.onValueChanged.AddListener(s => UpdateValue(!showingAsEuler, s, (v, old) => new Quaternion(old.x, v, old.z, old.w)));
+	quaternionZ.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	quaternionZ.onValueChanged.AddListener(s => UpdateValue(!showingAsEuler, s, (v, old) => new Quaternion(old.x, old.y, v, old.w)));
+	quaternionW.onSelect.AddListener(_ => isValueModificationOnGoing = true);
+	quaternionW.onValueChanged.AddListener(s => UpdateValue(!showingAsEuler, s, (v, old) => new Quaternion(old.x, old.y, old.z, v)));
+    }
+
+    void UpdateValue(bool pred, string newValue, Func<float, Quaternion, Quaternion> converter)
+    {
+	if (!pred)
+	{
+	    return;
+	}
+
+	isValueModificationOnGoing = false;
+	if (float.Parse(newValue) is float v)
+	{
+	    var currentValue = (Quaternion)Getter();
+	    Setter(converter(v, currentValue));
+	}
     }
 
     protected override void SetValue(Quaternion newValue)

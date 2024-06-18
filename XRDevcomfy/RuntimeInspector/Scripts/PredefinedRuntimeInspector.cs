2694fd4b02bbaa38f7f618731014f1f60d647a4d
Cj-bc <cj.bc-sd@outlook.jp>
2024-06-19 01:32:08 +0900

    feat: Make PredefinedRuntimeInspector holds list of MethodReference

--- XRDevcomfy/RuntimeInspector/Scripts/PredefinedRuntimeInspector.cs
+++ XRDevcomfy/RuntimeInspector/Scripts/PredefinedRuntimeInspector.cs
@@ -3,6 +3,7 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 **/
+using System;
 using System.Reflection;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,7 @@ namespace XRDevcomfy.RuntimeInspector
     public class PredefinedRuntimeInspector : MonoBehaviour
     {
 	public List<PropertyReference.PropertyReference> predefinedBindings;
+	public List<PropertyReference.MethodReference> predefinedMethodBindings;
 	/// Root Transform that contains all template instances
 	public Transform propertiesRoot;
 
@@ -26,6 +28,17 @@ namespace XRDevcomfy.RuntimeInspector
 		var instance = factory.Create(bind.Target, bind.PropertyInfo);
 		instance.transform.SetParent(propertiesRoot, false);
 	    }
+
+	    foreach (var bind in predefinedMethodBindings)
+	    {
+		var instance = factory.Create(bind.Target, bind.MethodInfo);
+		instance.transform.SetParent(propertiesRoot, false);
+	    }
+	}
+
+	public void Log()
+	{
+	    Debug.Log("Hi! Called!");
 	}
 
     }

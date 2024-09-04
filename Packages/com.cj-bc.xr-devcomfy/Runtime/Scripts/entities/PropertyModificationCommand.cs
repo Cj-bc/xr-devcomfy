93cf31b2f0948d2fde5de68aa8b9bf688ef6a3a4
Cj-bc <cj.bc-sd@outlook.jp>
2024-09-04 16:25:16 +0900

    feat: Implement Property modification as command to support Undo/redo

---
+++ Packages/com.cj-bc.xr-devcomfy/Runtime/Scripts/entities/PropertyModificationCommand.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Reflection;
+
+namespace XRDevcomfy
+{
+    /// Represents one modification procedure. It will be used for recording value diffs
+    public struct PropertyModificationCommand<T>
+    {
+        public MonoBehaviour target { get; private set; }
+        public PropertyInfo property { get; private set; }
+        public T oldValue { get; private set; }
+        public T newValue { get; private set; }
+
+        public PropertyModificationCommand(MonoBehaviour target, PropertyInfo prop, T oldVal, T newVal)
+        {
+            this.target = target;
+            this.property = prop;
+            this.oldValue = oldVal;
+            this.newValue = newVal;
+        }
+
+        public void Execute()
+        {
+            property.SetValue(target, newValue);
+        }
+
+        public void Undo()
+        {
+            property.SetValue(target, oldValue);
+        }
+    }
+
+}

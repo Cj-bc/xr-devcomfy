8f6352907c00f27b5d5ee84786c6f575170cdecd
Cj-bc <cj.bc-sd@outlook.jp>
2024-09-08 09:36:09 +0900

    feat(core): Add TransformGizmoController that connects TransformGizmo manipulation

---
+++ Packages/com.cj-bc.xr-devcomfy/Runtime/Scripts/adapters/TransformGizmoController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+using XRDevcomfy.Components;
+
+namespace XRDevcomfy
+{
+    /// Control transform
+    public class TransformGizmoController : IGameObjectSelectionController
+    {
+	private IGetTransformGizmoResultUseCase getGizmoResult;
+	private ISetTransformGizmoUseCase setGizmo;
+
+        private GameObject? SelectedGameObject;
+
+	public TransformGizmoController(IGetTransformGizmoResultUseCase _getGizmoResult,
+					ISetTransformGizmoUseCase _setGizmo)
+	{
+	    getGizmoResult = _getGizmoResult;
+	    setGizmo = _setGizmo;
+	}
+
+        public void SelectGameObject(GameObject obj)
+	{
+	    if (SelectedGameObject is not null
+		&& getGizmoResult.GetTransformGizmoResult() is (TransformValue before, TransformValue after))
+	    {
+		// Commit modification
+	    }
+
+	    SelectedGameObject = obj;
+	    setGizmo.SetTransformGizmoTo(SelectedGameObject.transform);
+	}
+    }
+}
